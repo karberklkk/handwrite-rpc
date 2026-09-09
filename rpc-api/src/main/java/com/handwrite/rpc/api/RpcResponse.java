7572508349c9145f53839c0633e139521e4d45cc
karberklkk <l861953338@163.com>
2026-09-09 21:01:43 +0800

    feat(api): add RpcRequest and RpcResponse message objects

---
+++ rpc-api/src/main/java/com/handwrite/rpc/api/RpcResponse.java
@@ -0,0 +1,48 @@
+package com.handwrite.rpc.api;
+
+import java.io.Serializable;
+
+public class RpcResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    // 成功时的返回值(远程方法执行的返回结果)
+    private Object data;
+    // 失败时的错误信息(不能吞异常!调用方要知道服务端出了什么问题)
+    private String errorMessage;
+
+    // 判断是否成功:errorMessage == null 即成功
+    public boolean isSuccess() {
+        return errorMessage == null;
+    }
+
+    // 无参构造 + 全参构造 + get/set + toString
+        public RpcResponse() {
+    }
+
+    public RpcResponse(Object data, String errorMessage) {
+        this.data = data;
+        this.errorMessage = errorMessage;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public String toString() {
+        return "RpcResponse{data=" + data + ", errorMessage='" + errorMessage + "'}";
+    }
+}
